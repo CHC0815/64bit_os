501b2c8f70c9ccdba36ec749fb46c5de9ee28760
Conrad Carl <coheca0815@gmail.com>
2021-08-25 22:44:48 +0200

    exception and interrupt handling

--- kernel.asm
+++ kernel.asm
@@ -2,7 +2,17 @@
 [ORG 0x200000]
 
 start:
+    mov rdi, Idt
+    mov rax, handler0
+
+    mov [rdi], ax
+    shr rax, 16
+    mov [rdi+6], ax
+    shr rax, 16
+    mov [rdi+8], rax
+    
     lgdt [Gdt64Ptr]
+    lidt [IdtPtr]
 
     push 8
     push KernelEntry
@@ -13,12 +23,22 @@ KernelEntry:
     mov byte[0xb8000], 'K'
     mov byte[0xb8001], 0xa
 
+    xor rbx, rbx
+    div rbx
+
+
 End:
     hlt
     jmp End
 
 
+handler0:
+    mov byte[0xb8000], 'D'
+    mov byte[0xb8001], 0xc
 
+    jmp End
+
+    iretq
 
 
 Gdt64:
@@ -29,3 +49,19 @@ Gdt64Len: equ $-Gdt64
 
 Gdt64Ptr: dw Gdt64Len-1
           dq Gdt64
+
+Idt:
+    %rep 256
+        dw 0
+        dw 0x8
+        db 0
+        db 0x8e
+        dw 0
+        dd 0
+        dd 0
+    %endrep
+
+IdtLen: equ $-Idt
+
+IdtPtr: dw IdtLen-1
+        dq Idt
