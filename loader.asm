4f91b732e2637e8938d8896b8a35a1f2e868652f
Conrad Carl <coheca0815@gmail.com>
2021-08-25 02:59:32 +0200

    Load kernel file

--- loader.asm
+++ loader.asm
@@ -15,6 +15,19 @@ start:
     test edx, (1<<26)
     jz NotSupport
 
+LoadKernel:
+    mov si, ReadPacket
+    mov word[si], 0x10      ; size 16 bytes
+    mov word[si+2], 100     ; number of sectors
+    mov word[si+4], 0       ; offset
+    mov word[si+6], 0x1000  ; segment       --> 0x1000:0 = 0x1000*16+0 = 0x10000
+    mov dword[si+8], 6      ; address lo
+    mov dword[si+0xc], 0    ; address hi
+    mov dl, [DriveId]
+    mov ah, 0x42
+    int 0x13
+    jc ReadError
+
     mov ah, 0x13
     mov al, 1
     mov bx, 0xa
@@ -23,11 +36,13 @@ start:
     mov cx, MessageLen
     int 0x10
 
+ReadError:
 NotSupport:
 End:
     hlt
     jmp End
 
 DriveId: db 0
-Message: db "Long mode is supported!"
+Message: db "Kernel is loaded!"
 MessageLen: equ $-Message
+ReadPacket: times 16 db 0       ; 0 size, 2 number of sectors, 4 offset, 6 segment, 8 address lo, 14 address hi
